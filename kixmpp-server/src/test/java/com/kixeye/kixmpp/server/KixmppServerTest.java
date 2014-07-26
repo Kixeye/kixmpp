ee296386ddb627b225ad69b85cbbee5264920e43
ebahtijaragic <elvir@kixeye.com>
2014-07-26 00:29:35 -0700

    Fixing tests.

--- kixmpp-server/src/test/java/com/kixeye/kixmpp/server/KixmppServerTest.java
+++ kixmpp-server/src/test/java/com/kixeye/kixmpp/server/KixmppServerTest.java
@@ -23,9 +23,28 @@ package com.kixeye.kixmpp.server;
 
 import io.netty.handler.ssl.SslContext;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.cert.X509Certificate;
+import java.util.Date;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+import org.bouncycastle.jce.X509Principal;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.MiscPEMGenerator;
+import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator;
+import org.bouncycastle.openssl.jcajce.JcaPKCS8Generator;
+import org.bouncycastle.operator.OutputEncryptor;
+import org.bouncycastle.util.io.pem.PemObjectGenerator;
+import org.bouncycastle.util.io.pem.PemWriter;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -45,10 +64,42 @@ import com.kixeye.kixmpp.server.module.auth.SaslKixmppServerModule;
  * 
  * @author ebahtijaragic
  */
+@SuppressWarnings("deprecation")
 public class KixmppServerTest {
 	@Test
 	public void testSimple() throws Exception {
-		try (KixmppServer server = new KixmppServer("testChat", SslContext.newClientContext())) {
+		Security.addProvider(new BouncyCastleProvider());
+		
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(1024, new SecureRandom());
+        KeyPair keyPair = keyGen.generateKeyPair();
+       
+        X509V3CertificateGenerator v3CertGen =  new X509V3CertificateGenerator();
+        v3CertGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
+        v3CertGen.setIssuerDN(new X509Principal("CN=cn, O=o, L=L, ST=il, C= c"));
+        v3CertGen.setNotBefore(new Date(System.currentTimeMillis() - 1000L * 60 * 60 * 24));
+        v3CertGen.setNotAfter(new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 365*10)));
+        v3CertGen.setSubjectDN(new X509Principal("CN=cn, O=o, L=L, ST=il, C= c"));
+        v3CertGen.setPublicKey(keyPair.getPublic());
+        v3CertGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+        X509Certificate cert = v3CertGen.generateX509Certificate(keyPair.getPrivate());
+        
+        File keyFile = File.createTempFile("key" ,null);
+        File certChainFile = File.createTempFile("cert" ,null);
+        
+        try (FileWriter writer = new FileWriter(keyFile)) {
+        	try (PemWriter pemWriter = new PemWriter(writer)) {
+            	pemWriter.writeObject(new JcaPKCS8Generator(keyPair.getPrivate(), null));
+        	}
+        }
+        
+        try (FileWriter writer = new FileWriter(certChainFile)) {
+        	try (PemWriter pemWriter = new PemWriter(writer)) {
+            	pemWriter.writeObject(new JcaMiscPEMGenerator(cert));
+        	}
+        }
+        
+		try (KixmppServer server = new KixmppServer("testChat", SslContext.newServerContext(certChainFile, keyFile))) {
 			Assert.assertNotNull(server.start().await(2, TimeUnit.SECONDS));
 			
 			server.module(SaslKixmppServerModule.class).addUser("testUser", "testPassword");
