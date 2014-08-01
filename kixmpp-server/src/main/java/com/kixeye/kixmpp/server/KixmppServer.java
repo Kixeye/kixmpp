9ef6f4b7fd4aed36110fb41da5355bb4df53f9fe
dturner <dturner@kixeye.com>
2014-08-01 14:43:22 -0700

    initial revision

--- kixmpp-server/src/main/java/com/kixeye/kixmpp/server/KixmppServer.java
+++ kixmpp-server/src/main/java/com/kixeye/kixmpp/server/KixmppServer.java
@@ -32,6 +32,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
@@ -160,6 +162,7 @@ public class KixmppServer implements AutoCloseable {
 			.channel(NioServerSocketChannel.class)
 			.childHandler(new ChannelInitializer<SocketChannel>() {
 				protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO));
 					ch.pipeline().addLast(new KixmppCodec());
 					ch.pipeline().addLast(new KixmppServerMessageHandler());
 				}
