904d2c663697d124af1cc0dc2676f86b0db88a4d
Elvir Bahtijaragic <ebahtijaragic@kixeye.com>
2014-09-18 17:53:28 -0700

    Fixing date format.

--- kixmpp-core/src/main/java/com/kixeye/kixmpp/date/XmppDateUtils.java
+++ kixmpp-core/src/main/java/com/kixeye/kixmpp/date/XmppDateUtils.java
@@ -32,7 +32,7 @@ import org.joda.time.format.DateTimeFormatter;
 public final class XmppDateUtils {
 	private XmppDateUtils() { }
 
-    private static final DateTimeFormatter xmppDateTimeFormatter = DateTimeFormat.forPattern("YYYY-MM-DD'T'hh:mm:ss[.sss]Z");
+    private static final DateTimeFormatter xmppDateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'hh:mm:ss[.sss]Z");
     
     /**
      * Format a long from epoch in UTC to a string.
